d3287eb681d467b0e5fd1244ce625f9a577730c6
Mark Redekopp <redekopp@usc.edu>
2024-11-03 16:48:53 -0800

    Added named containers and kill script

---
+++ u10-cloud/containers/kill-docker.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker kill app1 app2
+docker rm app1 app2
--- u10-cloud/containers/run-docker.sh
+++ u10-cloud/containers/run-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #docker run -it -p 5050:5000 my-flask-app
-docker run -d -p 5050:5000 my-flask-app
-docker run -d -p 5051:5000 my-flask-app
+docker run -d -p 5050:5000 --name app1 my-flask-app
+docker run -d -p 5051:5000 --name app2 my-flask-app
